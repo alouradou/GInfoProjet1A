8d91b1c8f8b05e867df171ac517b109d9139bdf9
louradoua <alou2000.78@gmail.com>
2021-03-03 14:40:18 +0100

    Implemented AdminBSB

--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -1,8 +1,8 @@
 /** VENDOR CSS **/
 require('bootstrap/dist/css/bootstrap.css');
-//require('adminbsb-materialdesign/scss/style.scss');
+require('adminbsb-materialdesign/scss/style.scss');
 require('bootstrap-select/dist/css/bootstrap-select.css');
-//require('adminbsb-materialdesign/scss/themes/_all-themes.scss');
+require('adminbsb-materialdesign/scss/themes/_all-themes.scss');
 require('animate.css');
 
 /** VENDOR JS **/

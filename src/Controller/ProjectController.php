d987853b9a8bdaddb34177e8aabedbe4590bbfbe
louradoua <alou2000.78@gmail.com>
2021-03-03 23:31:55 +0100

    Added fixtures

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
+use App\Entity\Item;
 use App\Entity\User;
+use phpDocumentor\Reflection\Types\True_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +15,7 @@ class ProjectController extends AbstractController
     /**
      * @Route("/", name="projet_claque")
      */
-    public function index(): Response
-    {
+    public function index(): Response{
         return $this->render('project/index.html.twig', [
             'controller_name' => 'ProjectController',
         ]);
@@ -22,8 +24,7 @@ class ProjectController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function home(): Response
-    {
+    public function home(): Response{
         $repo = $this->getDoctrine()->getRepository(User::class);
         $users = $repo->findAll();
         return $this->render('project/home.html.twig',[
@@ -32,5 +33,39 @@ class ProjectController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/utilisateurs", name="users")
+     */
+    public function users(): Response {
+        $repo = $this->getDoctrine()->getRepository(User::class);
+        $users = $repo->findAll();
+        return $this->render('project/users.html.twig',[
+            'controller_name' => 'ProjectController',
+            'users' => $users
+        ]);
+    }
 
+    /**
+     * @Route("/cours", name="courses")
+     */
+    public function courses(): Response {
+        $repo = $this->getDoctrine()->getRepository(Course::class);
+        $courses = $repo->findAll();
+        return $this->render('project/courses.html.twig', [
+            'controller_name' => 'ProjectController',
+            'courses' => $courses
+        ]);
+    }
+
+    /**
+     * @Route("/elements", name="items")
+     */
+    public function items(): Response {
+        $repo = $this->getDoctrine()->getRepository(Item::class);
+        $items = $repo->findAll();
+        return $this->render('project/courses.html.twig', [
+            'controller_name' => 'ProjectController',
+            'items' => $items
+        ]);
+    }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Course;
+use App\Entity\Item;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -12,6 +15,8 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
 
         $user = new User();
         $user->setFirstName("John")
@@ -32,6 +37,45 @@ class AppFixtures extends Fixture
             ->setUsername("tcook");
 
         $manager->persist($user2);
+
+        for ($i=0;$i<4;$i++){
+            $user = new User();
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($user->getFirstName().".".$user->getFirstName()."@centrale-marseille.fr")
+                ->setPromo(2020)
+                ->setPassword("azerty")
+                ->setUsername($user->getFirstName()[0].$user->getLastName());
+            $manager->persist($user);
+        }
+
+        $course2 =  new Course();
+        $course2->setCreatedBy($user2)
+            ->setName("Mon premier cours de Symfony")
+            ->setActive(true)
+            ->setOpen(false);
+        $manager->persist($course2);
+        for ($i=0;$i<2;$i++){
+            $course =  new Course();
+            $course->setCreatedBy($user)
+                ->setName("Mon premier cours de ".$faker->internetExplorer)
+                ->setActive(true)
+                ->setOpen(false);
+            $manager->persist($course);
+        }
+
+//        $item =  new Item();
+//        $item->setChapter($faker->word())
+//            ->setCourse($course2)
+//            ->setDescription($faker->word)
+//            ->setName($faker->name)
+//            ->setOrd($faker->randomDigit);
+//        $item2 =  new Item();
+//        $item2->setChapter($faker->word())
+//             ->setCourse($course2)
+//             ->setDescription($faker->word)
+//             ->setName($faker->name)
+//             ->setOrd($faker->randomDigit);
 
         $manager->flush();
     }
